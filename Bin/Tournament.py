bb6b16cc5a3610bc90d0c73af8e6d53a359486a8
VictoriaSedig <victoria.sedig@gmail.com>
2021-09-21 11:22:24 +0200

    pseudocode-for-torunament

--- Bin/Tournament.py
+++ Bin/Tournament.py
@@ -2,4 +2,31 @@
 
 class Tournament():
     def __init__(self):
+    
+        ### read game history 
+
+        ### let player make her/his move
+
+        ### store the data for that players move 
+
+        ### send updated data to al players
+
+        ## if game finish -> 
+            # let host decide point format? (not sure what this is for)
+            # let everyone know that game is over and the results
+            # return
         return
+
+
+
+def main():
+
+    ### prompt players to initiate game
+    tournament = Tounament()
+
+    # store the results in the tournament data
+    # tournament_data += tournament
+    # send out tournamnet data
+
+if __name__ == '__main__':
+    main()
